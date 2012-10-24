a5d0f522c82c805e52ce77234e62fa427912db02
Sandeep Singh <sandeepsinghmails@gmail.com>
2012-10-24 19:19:53 +0530

    Included "Mute" & "Max Vol" Functionalities

--- Vol_Controller.c
+++ Vol_Controller.c
@@ -16,22 +16,26 @@
  * GNU General Public License for more details.
 
  * You should have received a copy of the GNU General Public License
- * along with Alsa Volume Controller. If not, see <http://www.gnu.org/licenses/>.
+ * along with Volume Controller. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * To Compile:
+ * $ gcc -g -o alsa_controller Vol_Controller.c `pkg-config --libs --cflags gtk+-2.0`
  */
 
 #include <stdio.h>
-#include <stdlib.h>  
-#include <gtk/gtk.h> 
+#include <stdlib.h>  /* For system() Function */
+#include <gtk/gtk.h> /* GTK Library			  */
 
-#define MAXSIZE 500  
+#define MAXSIZE 500  /* Buffer Size 		  */
 
+/* Buffer to Store the String to be print in the Window */
 char Buffer[MAXSIZE];
 
 void GetSystemVolume(void);
 
-void IncreaseVolume(GtkWidget *widget, gpointer window_label)
+void increase(GtkWidget *widget, gpointer window_label)
 {
-	/* Now, actually increase the Sound Level by 1 */
+	/* Now, actually increase the Cound Level by 1 */
 	system("amixer --quiet set Master 1+");
 
 	/* Get System Volume */
@@ -41,9 +45,9 @@ void IncreaseVolume(GtkWidget *widget, gpointer window_label)
   	gtk_label_set_text(GTK_LABEL(window_label), Buffer);
 }
 
-void DecreaseVolume(GtkWidget *widget, gpointer window_label)
+void decrease(GtkWidget *widget, gpointer window_label)
 {
-	/* Now, actually decrease the Sound Level by 1 */
+	/* Now, actually decrease the Cound Level by 1 */
 	system("amixer --quiet set Master 1-");
 
 	/* Get System Volume */
@@ -53,6 +57,30 @@ void DecreaseVolume(GtkWidget *widget, gpointer window_label)
     gtk_label_set_text(GTK_LABEL(window_label), Buffer);
 }
 
+void maxvolume(GtkWidget *widget, gpointer window_label)
+{
+	/* Now, actually increase the Sound Level by Maximum */
+	system("amixer --quiet set Master 31+");
+
+	/* Get System Volume */
+	GetSystemVolume();
+
+	/* Print Buffer Contents in GTK Window */
+  	gtk_label_set_text(GTK_LABEL(window_label), Buffer);
+}
+
+void novolume(GtkWidget *widget, gpointer window_label)
+{
+	/* Set the Sound Level to Zero */
+	system("amixer --quiet set Master 31-");
+
+	/* Get System Volume */
+	GetSystemVolume();
+
+	/* Print Buffer Contents in GTK Window */
+  	gtk_label_set_text(GTK_LABEL(window_label), Buffer);
+}
+
 int main(int argc, char** argv) 
 {
 	GtkWidget *window_label;
@@ -60,6 +88,8 @@ int main(int argc, char** argv)
   	GtkWidget *window_frame;
   	GtkWidget *increase_vol;
   	GtkWidget *decrease_vol;
+  	GtkWidget *max_vol;
+  	GtkWidget *zero_vol;
 
 	/* Initialize the GTK+ Library */
   	gtk_init(&argc, &argv);
@@ -71,7 +101,7 @@ int main(int argc, char** argv)
   	gtk_window_set_title(GTK_WINDOW(window), "ALSA Volume Controller");
 
 	/* Set Default Size of the GTK Window */
-  	gtk_window_set_default_size(GTK_WINDOW(window), 460, 150);
+  	gtk_window_set_default_size(GTK_WINDOW(window), 560, 150);
 
 
   	window_frame = gtk_fixed_new();
@@ -85,8 +115,16 @@ int main(int argc, char** argv)
   	gtk_widget_set_size_request(decrease_vol, 80, 35);
   	gtk_fixed_put(GTK_FIXED(window_frame), decrease_vol, 50, 80);
 
+  	max_vol = gtk_button_new_with_label("MAX");
+  	gtk_widget_set_size_request(max_vol, 80, 35);
+  	gtk_fixed_put(GTK_FIXED(window_frame), max_vol, 165, 20);
+
+  	zero_vol = gtk_button_new_with_label("MUTE");
+  	gtk_widget_set_size_request(zero_vol, 80, 35);
+  	gtk_fixed_put(GTK_FIXED(window_frame), zero_vol, 165, 80);
+
     window_label = gtk_label_new("copyright@SandeepSingh"); 
-  	gtk_fixed_put(GTK_FIXED(window_frame), window_label, 190, 58); 
+  	gtk_fixed_put(GTK_FIXED(window_frame), window_label, 280, 58); 
 
   	gtk_widget_show_all(window);
 
@@ -94,10 +132,16 @@ int main(int argc, char** argv)
     G_CALLBACK (gtk_main_quit), NULL);
 
   	g_signal_connect(increase_vol, "clicked", 
-    G_CALLBACK(IncreaseVolume), window_label);
+    G_CALLBACK(increase), window_label);
 
   	g_signal_connect(decrease_vol, "clicked", 
-    G_CALLBACK(DecreaseVolume), window_label);
+    G_CALLBACK(decrease), window_label);
+
+  	g_signal_connect(max_vol, "clicked", 
+    G_CALLBACK(maxvolume), window_label);
+
+  	g_signal_connect(zero_vol, "clicked", 
+    G_CALLBACK(novolume), window_label);
 
   	gtk_main();
 
